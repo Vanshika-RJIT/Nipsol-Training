4b9a2cb8c96f8ac2104767eab21f8b118d5ab3d5
Vanshika-RJIT <vanshikasikarwar79@gmail.com>
2020-09-20 12:34:07 +0530

    Second Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,19 @@ import { Component, VERSION } from '@angular/core';
 })
 export class AppComponent  {
   name = 'Angular ' + VERSION.major;
+  button1=false;
+  button2=false;
+  toggle()
+  {
+          this.button1=!this.button1;
+          this.button2=false;
+
+  }
+   toggle1()
+  {
+          this.button2=!this.button2;
+          this.button1=false;
+
+  }
+
 }
