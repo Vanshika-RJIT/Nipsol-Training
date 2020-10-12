624279731bba4fc45933aca4cd3e9adc7b40f159
Vanshika-RJIT <vanshikasikarwar79@gmail.com>
2020-10-12 17:49:44 +0530

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule,ReactiveFormsModule} from '@angular/forms';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { AppRoutingModule,routingComponents } from './app-routing.module';
@@ -9,7 +9,7 @@ import { DataService } from './data.service';
 import { CovidMeterComponent } from './covid-meter/covid-meter.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule,AppRoutingModule,ReactiveFormsModule],
+  imports:      [ BrowserModule, FormsModule,AppRoutingModule,ReactiveFormsModule,  HttpClientModule],
   declarations: [ AppComponent, HelloComponent,routingComponents, CovidMeterComponent],
   bootstrap:    [ AppComponent ],
   providers:[DataService]
