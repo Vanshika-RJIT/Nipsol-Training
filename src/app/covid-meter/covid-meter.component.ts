dd8ce2b95bc69c8c738469dfd24f638133c4668b
Vanshika-RJIT <vanshikasikarwar79@gmail.com>
2020-10-14 00:54:37 +0530

    3 files modified

--- src/app/covid-meter/covid-meter.component.ts
+++ src/app/covid-meter/covid-meter.component.ts
@@ -9,12 +9,21 @@ import { DataCovidService } from '../datacovid.service';
 export class CovidMeterComponent implements OnInit {
   public countries=[];
   public worlddata;
+  public idselected:string;
+   OnIdSelected(event:any)
+  { this.idselected=event.target.value;
+   console.log( this.idselected);
+
+
+  }
   constructor(private dataCovid:DataCovidService) { }
 
   ngOnInit() {
     this.dataCovid.getCountries().subscribe(data=>this.countries=data);
     this.dataCovid.getWorldData().subscribe(data1=>this.worlddata=data1);
   }
+ 
+
   
 
 }
